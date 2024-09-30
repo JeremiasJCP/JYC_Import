a967d5e08f433dd38e87ef8dc9c73cef4e3e76ed
Jeremias Cordero <126274168+JeremiasJCP@users.noreply.github.com>
2024-09-30 21:34:35 +0200

    Se mejoraron las desplegables, ademas se elimino el JS de las desplegables de las preguntas frecuentes ya que los cambios a la misma se hicieron en CSS. Se borro todo eel contenido inecesario. Se agrego un parrafo de introduccion con su titulo

--- DesplegablePreguntasFrecuentes.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.pregunta').forEach(function(icono) {
-    icono.addEventListener('click', function() {
-        var respuesta = this.parentElement.nextElementSibling;
-        respuesta.classList.toggle('mostrar');
-        // this.textContent = this.textContent === '+' ? '-' : '+';
-    });
-});
